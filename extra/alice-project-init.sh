882f8687f2f9dbc7521f160c0bf0f033de65aeef
Nunuhara Cabbage <nunuhara@haniwa.technology>
2021-05-03 20:39:55 -0700

    Add alice-project-init.sh script
    Add a script for initializing a blank project targetting a specific VM
    version. This is mainly useful for xsystem4 development, where it can be
    used to create small test cases when working on a specific game.

---
+++ extra/alice-project-init.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#
+# This script initializes a project folder for a blank project targeting a
+# specific VM version. It should be passed the path to a .ain file which it
+# will read HLL declarations from to set up the project.
+#
+# Usage: alice-project-init path/to/file.ain
+#
+
+set -e
+
+if [[ $# -ne 1 ]]; then
+    echo "Wrong number of arguments"
+    echo "Usage: $0 <path-to-ain-file>"
+    exit 1
+fi
+
+set -x
+
+SRCAIN="$1"
+AINVER=$(alice ain dump --ain-version "$SRCAIN")
+BASE=$(basename "$SRCAIN")
+DIR=$(dirname "$SRCAIN")
+PROJECT_NAME="${BASE%.*}"
+
+mkdir -p out
+mkdir -p src/hll
+alice ain dump --hll "$SRCAIN" > src/hll/hll.inc
+mv ./*.hll src/hll/
+
+#
+# NOTE: Uncomment this line to copy the game directory into the output
+#       directory. This will allow for running the produced .ain file
+#       directly from the output directory.
+#
+#cp -r $DIR/* out/
+
+cat > "$PROJECT_NAME.pje" <<- EOF
+ProjectName = "$PROJECT_NAME"
+
+CodeName = "$PROJECT_NAME.ain"
+CodeVersion = "$AINVER"
+
+SourceDir = "src"
+OutputDir = "out"
+
+Source = {
+    "src.inc",
+}
+EOF
+
+cat > "src/src.inc" <<- "EOF"
+SystemSource = {
+    "hll/hll.inc",
+}
+
+Source = {
+    "main.jaf",
+}
+EOF
+
+cat > "src/main.jaf" <<- "EOF"
+int main(void)
+{
+    system.MsgBox("Hello, world!");
+    system.Exit(0);
+}
+EOF
